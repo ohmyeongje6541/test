df5e7e3590a76817d9b116ea33b99104c73ae396
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-09-17 16:14:36 +0900

    논리 연산자

---
+++ js/04-operator/main.js
@@ -0,0 +1,42 @@
+//논리 연산자
+//논리형(불리언형, true/false)을 기반으로한 연산자
+const trueData = true;
+const falseData = false;
+
+// && (AND)
+//2개의 논리형 데이터가 참이면 참(true)
+// 1개라도 논리형 데이터가 거짓이면 거짓(false)
+
+// 템플릿 리터럴에는 변수뿐만 아니라 표현식을 삽입 가능
+// 표현식 : 데이터를 생성하는 코드
+console.log(`true && true -> ${trueData && trueData}`);
+console.log(`true && false -> ${trueData && falseData}`);
+
+// || (OR)
+//1개의 논리형 데이터가 참이면 참(true)
+//2개의 논리형 데이터가 거짓이면 거짓(false)
+console.log(`true || false -> ${trueData || falseData}`);
+console.log(`false || false -> ${falseData || falseData}`);
+
+// ! (NOT)
+//1개의 논리형 데이터만 필요
+//논리형 데이터가 거짓이면 참(true)
+//논리형 데이터가 참이면 거짓(false) 생성
+console.log(` !true -> ${!trueData}`);
+console.log(` !false -> ${!falseData}`);
+
+// 같다 == Vs. === / 다르다 != Vs. !==
+// == / !=
+// 동등 비교
+// 값(데이터)만 비교
+const number = 1;
+const str = "1";
+
+const bool1 = number == str;
+console.log(bool1); //true
+
+// === / !==
+// 동등 비교
+// 데이터와 데이터의 자료형을 비교
+const bool2 = number === str;
+console.log(bool2); //false
