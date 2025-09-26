27c5a499c5ca5535483c069f4b813232a2329a88
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-09-26 17:16:59 +0900

    react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rule from "./components/JSX/Rule";
+import Interpolation from './components/JSX/Interpolation';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <div>
+      <Interpolation></Interpolation>
+    </div>
+  )
+}
---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Interpolation from './JSX/Interpolation'
+
+export default function Header() {
+  return (
+    <div>
+      <Header></Header>
+      <Interpolation></Interpolation>
+    </div>
+  )
+}
