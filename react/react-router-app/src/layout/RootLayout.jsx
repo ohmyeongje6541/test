a5bf848e99285cad08d582148ba78095e66efc07
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-10-14 14:32:49 +0900

    constant-path

--- react/react-router-app/src/layout/RootLayout.jsx
+++ react/react-router-app/src/layout/RootLayout.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import PATHS from "../constants/path";
 
 export default function RootLayout () {
   return (
     <div>
       <div className="">
-        <Link to="/">홈페이지</Link>
-        <Link to="/abot">소개페이지</Link>
-        <Link to="/profile">사용자 정보 페이지</Link>
-        <Link to="/auth">인증 페이지</Link>
-        <Link to="/posts">게시글 페이지</Link>
+        <Link to={PATHS.ROOT.INDEX}>홈페이지</Link>
+        <Link to={PATHS.ROOT.ABOUT}>소개페이지</Link>
+        <Link to={PATHS.ROOT.PROFILE}>사용자 정보 페이지</Link>
+        <Link to={PATHS.AUTH.INDEX}>인증 페이지</Link>
+        <Link to={PATHS.ROOT.POSTS}>게시글 페이지</Link>
       </div>
       <Outlet></Outlet>
     </div>
