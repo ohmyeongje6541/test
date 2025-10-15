086cc28739898606036a66af04e4f42406eaeaa5
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-10-15 09:48:52 +0900

    redux-app

--- react/react-router-app/src/router/index.js
+++ react/react-router-app/src/router/index.js
@@ -2,10 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 
 import rootRoutes from "../routes/rootRoutes";
 import authRoutes from "../routes/authRoutes";
+import Notfound from "../pages/Notfound";
 
 
 const router = createBrowserRouter([
   ...rootRoutes,
   ...authRoutes,
+  {
+    path: "*", // 별표 : 모든 것에 매핑(일치)되는 특수문자
+    Component: Notfound,
+  }
 ]);
 export default router;
